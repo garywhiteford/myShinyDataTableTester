51ba787d87fa2d6345a9df7e992c8a2d17bf2db9
Gary Whiteford (10.196.101.22) <gwhiteford@carlsonwagonlit.com>
2018-02-15 00:03:00 +0100

    Add plot to sample app02

--- app02 - Percents.R
+++ app02 - Percents.R
@@ -33,8 +33,20 @@ server <- function(input, output) {
                                   stringsAsFactors = FALSE))")
   
   output$vrb_out2b <- renderText("renderFormattable(df_data2) > formattableOutput(\"tbl_out2\")")
-
+  output$vrb_out2c1 <- renderText(paste0("str(df_data2$", input$sel_val1, ")"))
+  output$vrb_out2c2 <- renderPrint({str(df_data2[[input$sel_val1]])})
+  output$vrb_out2d1 <- renderText(paste0("str(as.numeric(df_data2$", input$sel_val1, "))"))
+  output$vrb_out2d2 <- renderPrint({str(as.numeric(df_data2[[input$sel_val1]]))})
+  
   output$tbl_out2 <- renderFormattable(df_data2)
+  
+  output$plt_out2 <- renderPlot({
+    barplot(as.numeric(df_data2[[input$sel_val1]]), 
+            names.arg = df_data2$col_String, 
+            main=input$sel_val1,
+            ylab="Y axis label (value)",
+            xlab="X axis label")
+  })
 }
 
 ui <- fluidPage(
@@ -58,9 +70,17 @@ ui <- fluidPage(
       h3("Sample #4"),
       p("Note: Using the formattable functions ", em("accounting()"), " and ", em("percent()"), " along with some 
          formatting arguments provides the desired results. The format argument takes values that work with the ", 
-        em("formatC"), " function (e.g., \"d\" = integer)."),
+        em("formatC"), " function (e.g., \"d\" = integer). "),
       verbatimTextOutput("vrb_out2b"),
-      formattableOutput("tbl_out2")
+      p("Data in formattable.dataframe may be used in plots, ... "),
+      verbatimTextOutput("vrb_out2c1"),
+      verbatimTextOutput("vrb_out2c2"),
+      p("... but must first be coerced to a vector."),
+      verbatimTextOutput("vrb_out2d1"),
+      verbatimTextOutput("vrb_out2d2"),
+      formattableOutput("tbl_out2"),
+      selectInput("sel_val1", label = "Select value set", choices = names(df_data2[2:4])),
+      plotOutput("plt_out2")
     )
   )
 )
